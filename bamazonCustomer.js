229fd5b5b81a02fb1384907d7c99ff734b6a7ce7
Mohak Tamhane <tamhane.mohak@gmail.com>
2019-11-13 15:30:59 -0500

    shop products functionality implemented

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,14 +11,48 @@ const connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
     listAllProducts();
-    connection.end()
 });
 
 function listAllProducts() {
     connection.query("SELECT item_id, product_name, department_name, price, stock_quantity FROM products", function (err, response) {
         if (err) throw err;
         console.table(response);
+        shopProducts();
     });
+}
+
+function shopProducts() {
+    inquirer.prompt([
+        {
+            name: "item_id",
+            type: "input",
+            message: "Enter the item_id for the product you would like to purchase ?"
+        },
+        {
+            name: "quantity",
+            type: "input",
+            message: "How many would you like to purchase ?"
+        }]).then(function (response) {
+            var item_id = response.item_id;
+            var quantity = response.quantity;
+            var total;
+            var updateQuantity;
+
+            connection.query("SELECT * FROM products WHERE ?", { item_id: item_id }, function (err, response) {
+                if (err) throw err;
+                if (quantity > response[0].stock_quantity) {
+                    console.log("\nInsufficient Quantity!")
+                } else {
+                    total = quantity * response[0].price;
+                    updateQuantity = response[0].stock_quantity - quantity;
+                    console.log("\nYou purchased " + quantity + " " + response[0].product_name + "\n\nYour total is: $" + total);
+                    connection.query("UPDATE products SET ? WHERE ?", [{ stock_quantity: updateQuantity }, { item_id: item_id }], function (err, response) {
+                        if (err) throw err;
+                        console.log("\nInventory Updated")
+                    })
+                }
+                connection.end();
+            })
+        })
 }
