1ab347e3b9a94f9f38dd7495e75de877ff1275ba
Mohak Tamhane <tamhane.mohak@gmail.com>
2019-11-13 14:45:27 -0500

    display all products in database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,1 +1,24 @@
+var inquirer = require("inquirer");
+var mysql = require("mysql");
 
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "YOUR_MySQL_PASSWORD_GOES_HERE",
+    database: "bamazon"
+})
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    listAllProducts();
+    connection.end()
+});
+
+function listAllProducts() {
+    connection.query("SELECT item_id, product_name, department_name, price, stock_quantity FROM products", function (err, response) {
+        if (err) throw err;
+        console.table(response);
+    });
+}
